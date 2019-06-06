85952e97979005eab07f591aa816282233a52b3c
yara-andrian <andrian.kanta@yara.com>
2019-06-06 08:12:09 +0800

    updated the client

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Balance from './containers/Balance'
 import Index from './containers/Index'
 
 function AppRouter() {
@@ -8,7 +7,6 @@ function AppRouter() {
     <Router>
       <div>
         <Route path="/" exact component={Index} />
-        <Route path="/balances" component={Balance} />
       </div>
     </Router>
   );
